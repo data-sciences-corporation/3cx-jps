391420efd1f160124bb50ad04e41832c23cfd9fd
Richard Raymond <44724035+Umlatt@users.noreply.github.com>
2022-03-02 20:44:10 +0200

    Added autocleanup to updater

--- scripts/update3cx.sh
+++ scripts/update3cx.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 public_ip=$1
 fqdn=$2
-mkdir 3cx
+echo "Stopping current container" >> INSTALL_LOG
+docker stop 3cx
+echo "Removing previous _OLD version" >> INSTALL_LOG
+docker rm 3cx_old
+for image in `docker image list | grep '<none>' | awk '{print $3}'`; do docker rmi $image; done
+echo "Renaming current version to _OLD" >> INSTALL_LOG
+docker rename 3cx 3cx_old
+echo "Get and run latest version of 3cx container on IP [$public_ip] and URL [$fqdn]" >> INSTALL_LOG
 docker run \
   -d  \
   -t \
@@ -16,7 +23,11 @@ docker run \
   -p      5060:5060/udp \
   -p      5090:5090 \
   -p      5090:5090/udp \
-  -v    /root/3cx:/mnt/3cx \
+  -v    /root/3cx/data:/mnt/3cx \
+  -v    /root/3cx/config:/etc/3cxpbx
   --env CX_PUBLIC_IP=$public_ip \
   --env CX_INTERNAL_FQDN=$fqdn \
+  --name 3cx \
+  --restart unless-stopped \
           ghcr.io/izer-xyz/3cx:latest
+ echo "Update complete" >> INSTALL_LOG
