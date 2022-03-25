12aa7bbae667cb5c57987486faab06586b7e5553
Richard Raymond <richard.raymond@datasciences.co.za>
2022-03-25 14:19:55 +0200

    Revertme - added macvlan for test

--- scripts/deploy3cx.sh
+++ scripts/deploy3cx.sh
@@ -8,12 +8,23 @@ mkdir -p /root/3cx/mount
 mkdir -p /root/3cx/config
 chown -R docker. /root/3cx
 echo "[$script] Deploying container on IP [$public_ip] and URL [$fqdn]"  >> $logfile
+
+echo "[$script] Creating 3cx macvlan network" >> $logfile
+if [[ $(docker network list | grep mv_3cx) ]]; then 
+  echo "Network already exists." >> $logfile
+else 
+  echo "Creating a macvlan network for 3cx" >> $logfile
+  docker network create -d macvlan --subnet 192.168.1.0/24 --gateway 192.168.1.1 -o parent=venet0 mv_3cx
+fi
+
+echo "Deploying container."  >> $logfile
 docker run \
   -d  \
   -t \
   --tmpfs /tmp \
   --tmpfs /run \
-  --tmpfs /run/lock \
+  --tmpfs /run/lock 
+  --network mv_3cx
   -v      /sys/fs/cgroup:/sys/fs/cgroup:ro \
   -p      5015:5015 \
   -p      5000:5000 \
@@ -30,4 +41,4 @@ docker run \
   --name 3cx \
   --restart unless-stopped \
           ghcr.io/izer-xyz/3cx:latest
- echo "[$script] Docker deployment complete" >> $logfile
+echo "[$script] Docker deployment complete" >> $logfile
